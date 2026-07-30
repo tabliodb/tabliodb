a52ee98a2ddff72a7da7977867113764725d3fef
Rigent Tio Salma <rigentjogja@gmail.com>
2026-07-30 12:58:36 +0700

    feat: add Nunito Variable font support and update CSS variables

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+// Nunito dimuat dari bundle lokal agar self-hosted deployment tidak bergantung pada request Google Fonts.
+import '@fontsource-variable/nunito';
 import './styles/app.css';
 import { App } from './app/App';
 import { bootstrapApp } from './app/bootstrap/bootstrapApp';
